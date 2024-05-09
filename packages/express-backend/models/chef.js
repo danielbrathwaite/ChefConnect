688eb815a6003e2a7a0df4ff669db80dfda340a1
5phun <90543875+5phun@users.noreply.github.com>
2024-05-09 16:58:14 -0700

    adding chef schema

---
+++ packages/express-backend/models/chef.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const ChefSchema = new mongoose.Schema(
+  {
+    chefId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+    password: {
+        type: String,
+        required: true
+      },
+    firstName: {
+        type: String,
+        required: true
+      },
+    lastName: {
+        type: String,
+        required: true
+      },
+    address: {
+        type: String,
+        required: false
+    },
+    phoneNumber: {
+        type: String,
+        required: false
+    },
+    specialties: {
+        type: [String],
+        required: false
+    },
+    menu: { //adding menuItem collection later 
+        type: [String],
+        required: false
+    },
+    rating: {
+      type: Number,
+      required: false
+    }
+  },
+  { collection: "chefs_list" }
+);
+
+const Chef = mongoose.model("Chef", ChefSchema);
+
+export default Chef;
