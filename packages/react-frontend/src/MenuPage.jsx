cae82d2c57965383fd0c2d188faa2e9e74caec29
msri-02 <msrikant@calpoly.edu>
2024-06-01 18:02:43 -0700

    carousel code/style needed

--- packages/react-frontend/src/MenuPage.jsx
+++ packages/react-frontend/src/MenuPage.jsx
@@ -1,15 +1,35 @@
 import { useEffect } from "react";
 import Table from "./Table";
 import { useLocation } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MenuPageHeader({ chef })
 {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10px',
+  };
+
   return(
     <div className="container">
         <h2>
           Chef {chef.firstName} {chef.lastName}'s Menu 
         </h2>
         <img src={chef.profilePicture} className="chef-image"/>
+        <Slider {...settings}>
+          {chef.foodGallery.map((imageUrl, index) => (
+            <div className="carousel-item" key={index}>
+              <img src={imageUrl} alt={`Image ${index}`} />
+            </div>
+       ))}
+    </Slider>
     </div>
   );
 }
